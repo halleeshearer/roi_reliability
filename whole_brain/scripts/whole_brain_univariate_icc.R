f26a83b1d691fad507f06ff9dfe7c5e97b394faa
Hallee Shearer <89100433+halleeshearer@users.noreply.github.com>
2024-02-09 12:12:46 -0500

    Update whole_brain_univariate_icc.R
    fixed a bug where I was missing the first edge in the data dataframe

--- whole_brain/scripts/whole_brain_univariate_icc.R
+++ whole_brain/scripts/whole_brain_univariate_icc.R
@@ -14,7 +14,7 @@ conditions <- ["M", "R"]
 
 for (cond in conditions) {
   this_cond <- data[data[[2]]==cond,]
-  data <- as.matrix(this_cond[,5:dim(this_cond)[2]])
+  data <- as.matrix(this_cond[,4:dim(this_cond)[2]])
   subID <- as.matrix(this_cond[,1])
   session <- as.matrix(this_cond[,3])
 
